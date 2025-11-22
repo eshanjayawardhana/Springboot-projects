e4f2da1061fc361cfba7d56a2fd6f4db43295387
Eshan Jayawardana <thetej4@gmail.com>
2025-11-22 20:38:13 +0530

    Add Spring Boot application setup and initial configurations
    - Implement main application class with a startup message.
    - Configure database properties in application.properties.
    - Create initial structure for Employee-related classes (DTO, Entity, Repository, Service).

--- spring-jpa/spring-jpa/src/main/java/com/spring/spring_jpa/SpringJpaApplication.java
+++ spring-jpa/spring-jpa/src/main/java/com/spring/spring_jpa/SpringJpaApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJpaApplication {
+public class SpringJpaApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaApplication.class, args);
+		System.out.println("------- Running --------");
 	}
 
 }
